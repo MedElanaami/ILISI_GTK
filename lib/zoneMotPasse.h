b33d45d9893db0b79f1fa84902ecb8b142c7c3b6
MedElanaami <elanaamimohamed@gmail.com>
2024-03-04 01:06:54 +0100

    Ajouter structure Zone Mot Passe

---
+++ lib/zoneMotPasse.h
@@ -0,0 +1,19 @@
+//
+// Created by med on 3/4/24.
+//
+
+#ifndef ILISI_GTK_ZONEMOTPASSE_H
+#define ILISI_GTK_ZONEMOTPASSE_H
+#include "global.h"
+//Structure pour la zone de mot de passe
+typedef struct
+{ GtkWidget* mot_pass; //Le box
+    GtkWidget* conteneur;//l’élément qui va contenir la zone de mot de passe
+    gint max_car;// le maximum des caractères possibles à saisir
+    gint visible;// visible (1) ou pas(0)
+    gchar car_vis;// le caractère visible dans la zone
+    gchar* password;
+}password_object;
+
+
+#endif //ILISI_GTK_ZONEMOTPASSE_H
