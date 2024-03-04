6e5a3e500ad44f927c6d4af84209825975c5a8cf
MedElanaami <elanaamimohamed@gmail.com>
2024-03-04 01:58:48 +0100

    Some Updates

--- lib/box.h
+++ lib/box.h
@@ -1,13 +1,7 @@
-//
-// Created by med on 3/4/24.
-//
 
 #ifndef ILISI_GTK_BOX_H
 #define ILISI_GTK_BOX_H
-
-
 #include "global.h"
-
 ///*************************box***************************************************
 ///structure de box
 typedef struct
