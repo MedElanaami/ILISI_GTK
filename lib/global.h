ecafec806b18d9705a49627b6ba9db5259930093
MedElanaami <elanaamimohamed@gmail.com>
2024-03-04 01:32:53 +0100

    Some updates

--- lib/global.h
+++ lib/global.h
@@ -1,11 +1,16 @@
-//
-// Created by med on 3/3/24.
-//
 
 #ifndef ILISI_GTK_GLOBAL_H
 #define ILISI_GTK_GLOBAL_H
 #include <gtk/gtk.h>
 #include "fenetre.h"
-#include "structure.h"
+#include "Position.h"
 #include "app.h"
+#include "Taille.h"
+#include "bouton.h"
+#include "box.h"
+#include "zoneMotPasse.h"
+#include "zoneTexte.h"
+#include "FormatageDeTexte .h"
+
+
 #endif //ILISI_GTK_GLOBAL_H
